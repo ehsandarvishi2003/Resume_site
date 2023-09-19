22e1f76a49c001b4eb65e2254ecb304df9aa278a
EH$AN <laptop-karina@ehsan>
2023-09-19 21:00:52 +0330

    add admin panel not complitchange icons properties

---
+++ Resume_site/Controllers/AdminPanelController.cs
@@ -0,0 +1,13 @@
+﻿#region Using
+using Microsoft.AspNetCore.Mvc;
+#endregion
+namespace Resume_site.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
